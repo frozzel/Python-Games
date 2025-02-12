62cf3f9d8694892e52eff099aba63b8a68b680ca
Dennis Hickox <frozzel@me.com>
2025-02-12 15:58:58 -0500

    band name generator

---
+++ BandGennerator/main.py
@@ -0,0 +1,7 @@
+print("👨‍🎤 Welcome to Band Name Generator 🤘")
+
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + city + " " + pet + "\n" )
+
+print("Rock on 🎸 " + city + " " + pet)
---
+++ BandGennerator/scrypt.py
@@ -0,0 +1,31 @@
+# New Code for BandGenerator
+
+print("Hello World \nWelcome to BandGenerator")
+
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+print("2. Knead the dough for 10 minutes.")
+print("3. Add 3g of Salt.")
+print("4. Leave to rise for 2 hours.")
+print("5. Bake at 200 degrees C for 30 minutes.")
+
+print("Notes from Day 1")
+print("The print statement is used to output strings")
+print("Strings are strings of characters")
+print("String Concatenation is done with the" + " sign")
+print("New lines can be created with a \n and the letter n")
+
+name = input("Whats your name? ")
+print("Hello " + name + "!")
+
+glass1 = "Water"
+glass2 = "Milk"
+
+print("Glass 1: " + glass1)
+print("Glass 2: " + glass2)
+
+glass3 = glass1
+glass1 = glass2
+glass2 = glass3
+
+print("Glass 1: " + glass1)
+print("Glass 2: " + glass2)
