a79e2b4bad47a3aa54ec3916cb17ea5c6e869c92
Dennis Hickox <frozzel@me.com>
2025-02-12 23:59:57 -0500

    Treasure Island

---
+++ TreasureIsland/main.py
@@ -0,0 +1,22 @@
+with open("treasure.txt") as file:
+    contents = file.read()
+    print(contents)
+    print("Welcome to Treasure Island")
+    print("Your mission is to find the treasure.")
+    first_direction = input("You're at a cross road. Where do you want to go? Type 'left' or 'right'\n")
+    if first_direction.lower() == "left":
+        second_direction = input("You've come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across.\n")
+        if second_direction.lower() == "wait":
+            third_direction = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?\n")
+            if third_direction.lower() == "red":
+                print("It's a room full of fire. 🔥🔥🔥 Game Over.")
+            elif third_direction.lower() == "yellow":
+                print("💰 You found the treasure! 💰 You Win!")
+            elif third_direction.lower() == "blue":
+                print("You enter a room of beasts. 🧌  Game Over.")
+            else:
+                print("You chose a door that doesn't exist. 👽 aliens kill you! Game Over.")
+        else:
+            print("You get attacked by an angry shark. 🦈 Game Over.")
+    else:
+        print("You fell into a hole. 🕳️  Game Over.")
--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ my_name = "Dennis"
 print("Hello, " + my_name + "!")
 
 # This variable will be used to count the number of times anyone tweets the word persnickety
-persnickety_count = 0
+persnickety_count = 4.5684846
 
 # This code will calculate the likelihood that it will rain tomorrow
 # complicated_rain_calculation_for_tomorrow()
@@ -11,4 +11,9 @@ persnickety_count = 0
 # useful_value = new_clean_code()
 
 # from Mary Shelley's Frankenstein
-print(my_name + ", there is something at work in my soul, which I do not understand." )
+print(f"{my_name}, there is something at work in my soul, which I do not understand. " + str(persnickety_count + 4))
+print(my_name[0])
+print(123+456)
+print(int("123456")+ 6)
+print(int(3*(3 + 3)/3 - 3))
+print(round(persnickety_count, 4))
