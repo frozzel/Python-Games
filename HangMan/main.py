8626f5eb7547cc2a231ea206c0cefb6ccb606fa2
Dennis Hickox <frozzel@me.com>
2025-02-14 01:01:17 -0500

    hangman'

--- HangMan/main.py
+++ HangMan/main.py
@@ -1,4 +1,6 @@
 from wonderwords import RandomWord
+from test import hangman
+import os
 
 with open("hangman.txt") as file:
     content = file.read()
@@ -20,16 +22,41 @@ for letter in chosen_word:
     placeholder += "_"
 
 print("Word to guess: " + placeholder)
-lives = 6
-display = ""
 
-while lives > 0:
+game_over = False
+lives = 6
+stage = 0
+
+correct_guess = []
+
+while not game_over:
     guess = input("Guess a letter: ").lower()
+    display = ""
+    os.system("clear")
     for letter in chosen_word:
         if letter == guess:
             display += letter
-        
+            correct_guess.append(guess)
+        elif letter in correct_guess:
+            display += letter
         else:
-            display += "_"
+            display += "_" 
+    if guess not in chosen_word:
+        lives -= 1
+        stage += 1  
     print(display)
+    print(hangman[stage])
+    print(f"Lives: {lives}")
+    
+    if lives == 0:
+        print("You Lose!")
+        game_over = True
+        with open("death.txt") as file:
+            death = file.read()
+            print(death)
+    if display == chosen_word:
+        print("You Win!")
+        game_over = True
+
+        
      
