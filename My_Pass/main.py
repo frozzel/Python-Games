c77f14ca00a90566ff245988c455d8c088dfac8c
Dennis Hickox <frozzel@me.com>
2025-03-11 13:19:03 -0400

    align config East

--- My_Pass/main.py
+++ My_Pass/main.py
@@ -24,20 +24,20 @@ canvas.create_image(100, 100, image=lock_image)
 canvas.grid(column=1, row=0)
 
 # ---------------------------- LABELS ------------------------------- #
-website_label = Label(text="Website:", fg=BLACK, bg=WHITE, justify="right")
-website_label.grid(column=0, row=1, )
+website_label = Label(text="Website:", fg=BLACK, bg=WHITE,)
+website_label.grid(column=0, row=1, sticky="E" )
 
 website_input = Entry(width=35, fg=BLACK, bg=WHITE, highlightthickness=0 )
 website_input.grid(column=1, row=1, columnspan=2)
 
 email_label = Label(text="Email/Username:", fg=BLACK, bg=WHITE, justify="right")
-email_label.grid(column=0, row=2)
+email_label.grid(column=0, row=2, sticky="E")
 
 email_input = Entry(width=35, fg=BLACK, bg=WHITE, highlightthickness=0)
 email_input.grid(column=1, row=2, columnspan=2)
 
 password_label = Label(text="Password:", fg=BLACK, bg=WHITE)
-password_label.grid(column=0, row=3)
+password_label.grid(column=0, row=3, sticky="E")
 
 password_input = Entry(width=19, fg=BLACK, bg=WHITE, highlightthickness=0)
 password_input.grid(column=1, row=3)
