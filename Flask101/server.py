6bf8279551eb72f0a400e8562a1d597a8d5fa7b2
Dennis Hickox <frozzel@me.com>
2025-04-09 22:28:04 -0400

    init flask;

--- Flask101/server.py
+++ Flask101/server.py
@@ -5,7 +5,10 @@ app  = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World!"
+    return "<h1 style='text-align: center'>Hello, World!</h1>" \
+              "<p style='text-align: center'>This is a simple Flask application.</p>" \
+                "<p style='text-align: center'>You can access different pages using the links below:</p>" \
+                    "<img src='https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExdm9qaDVtaTl1NXhtaHRvZXllMTFhaWQ5cnF1Y3VvZ3I1OTN3endvbSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/njtPBlbYnHAHK/giphy.gif' alt='Kitten' width: 200px>" \
 
 @app.route('/about')
 def about():
@@ -14,6 +17,10 @@ def about():
 @app.route('/contact')
 def contact():
     return "This is the contact page. carl me at 1234567890"
+
+@app.route('/username/<name>') # <name> is a variable part of the URL
+def greater(name):
+    return f"Hello {name}!"
 
 if __name__ == '__main__':
     # app.run(host="0.0.0.0", port=8080)
