1eb132127fe02d6c7538a2541aadcb4950c9ce0e
Dennis Hickox <frozzel@me.com>
2025-04-18 14:11:18 -0400

    blogger update to contact form

--- Blogger/server.py
+++ Blogger/server.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 import datetime
+import smtplib
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+
 
 app = Flask(__name__)
 
@@ -15,15 +21,30 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    return render_template('contact.html')
-
-# @app.route('/post')
-# def post():
-#     res = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
-#     posts = res.json()
-#     return render_template('post.html', posts=posts)
+    
+    def send_email(name, email, phone, message):
+        my_email = os.getenv("EMAIL")
+        my_password = os.getenv("PASSWORD")
+        with smtplib.SMTP('smtp.gmail.com', 587) as conn:
+            conn.starttls()
+            conn.login(user=my_email, password=my_password)
+            conn.sendmail(from_addr=email, to_addrs=my_email,
+                          msg=f"Subject:New message from {name}\n\nYou got a new message from Blogger Site:\n From: {name}\n Email: {email}\nPhone: {phone}\nMessage:\n{message}")
+            
+            
+    if request.method == 'POST':
+        data = request.form
+        name = data.get('name')
+        email = data.get('email')
+        message = data.get('message')
+        # Here you would typically send the message to an email or store it in a database
+        phone = data.get('phone')
+        # Send email
+        send_email(name, email, phone, message)
+        return render_template('contact.html', msg_sent=True)
+    return render_template('contact.html', msg_sent=False)
 
 @app.route('/<int:post_id>')
 def post_detail(post_id):
